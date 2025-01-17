c4f79f751341f8f1b339b6bfb4f93567064a6ea1
root <menheimgeorges83@gmail.com>
2025-01-17 22:51:57 +0100

    correction

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    # Récupérer tous les noms définis dans le module hidden_4
+    names = dir(hidden_4)
+    # Filtrer et trier les noms
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
