b9457eaaf86f37f4c868fe1b3b9636c3cac32280
georges479 <g.ismael071221@gmail.com>
2025-05-20 14:16:20 +0200

    creation of all files and task 0 to 2 finished

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module defines an empty class named Square."""
+
+class Square:
+    """This class represents a square. Currently, it has no attributes or methods."""
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module defining a Square class."""
+
+class Square:
+    """Class that defines a square by its size."""
+    def __init__(self, size):
+        """Initialize the square with a private size attribute."""
+        self.__size = size
