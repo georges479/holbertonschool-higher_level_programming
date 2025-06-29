83b299e1b72f312c93029c99b8ac59883aa74a86
georges479 <g.ismael071221@gmail.com>
2025-06-29 16:26:38 +0200

    Task 1 to 16

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create en neww database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Create a table named `first_table` with two columns:
+-- `id` of type `INT`
+-- `name` of type `VARCHAR(256)`
+CREATE TABLE first_table (
+    id INT,
+    name VARCHAR(256)
+);
