6141dc7916ddda453642b75b9d051d4efa9e7be4
georges479 <g.ismael071221@gmail.com>
2025-08-06 19:52:23 +0200

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    # Récupérer tous les noms définis dans le module hidden_4
     names = dir(hidden_4)
-    # Filtrer et trier les noms
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
