56cc41855b4e5544ee6978534d8cbb40fab79780
georges479 <g.ismael071221@gmail.com>
2025-08-06 17:34:49 +0200

    corection task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Lists all states from the database hbtn_0e_0_usa.
+
+Usage:
+    ./0-select_states.py <mysql username> <mysql password> <database name>
+
+This script connects to a MySQL database using credentials
+provided as command-line arguments, then fetches and prints
+all rows from the 'states' table.
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Connect to the MySQL database using command line arguments
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    cursor = db.cursor()
+    
+    # Execute SQL query to select all data from states table
+    cursor.execute("SELECT * FROM `states`")
+    
+    # Fetch all results and print each state
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
