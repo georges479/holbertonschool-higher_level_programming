a4dffc2863e8f5d796fae8ad232ba9a763b23d92
georges479 <g.ismael071221@gmail.com>
2025-08-06 18:20:04 +0200

    pycodestyle task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,8 @@ Displays all values in the states table of the database hbtn_0e_0_usa
 where name matches the argument.
 
 Usage:
-    ./script.py <mysql username> <mysql password> <database name> <state name searched>
+    ./script.py <mysql username> <mysql password>
+    <database name> <state name searched>
 
 Connects to localhost on port 3306.
 Uses string formatting to create the SQL query.
