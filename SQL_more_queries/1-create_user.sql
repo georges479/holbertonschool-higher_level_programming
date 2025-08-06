c2f34733041bbb6b97b7327bd09f6a34c75ebca3
georges479 <g.ismael071221@gmail.com>
2025-08-06 19:19:15 +0200

    correction task 1.2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates the user 'user_0d_1' if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grants all privileges to 'user_0d_1' on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Applies the privilege changes
 FLUSH PRIVILEGES;
--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,11 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+-- Crée la base de données hbtn_0d_2 si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Crée l'utilisateur user_0d_2 si il n'existe pas
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Donne uniquement le privilège SELECT sur la base hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Recharge les privilèges pour qu'ils soient pris en compte immédiatement
 FLUSH PRIVILEGES;
